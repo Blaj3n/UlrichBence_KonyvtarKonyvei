1731fcc155a38782477aaaa8a908c93e52edf01c
Blaj3n <benceulrich@gmail.com>
2023-04-25 12:55:02 +0200

    Könyv és Könyvtár java kész class

--- src/modell/Konyv.java
+++ src/modell/Konyv.java
@@ -3,6 +3,21 @@ package modell;
 public class Konyv {
 
 
+    String cim, szerzo1, szerzo2;
+    int kiadasi_ev;
+
+    public Konyv(String cim, String szerzo1, String szerzo2, int kiadasi_ev) {
+        this.cim = cim;
+        this.szerzo1 = szerzo1;
+        this.szerzo2 = szerzo2;
+        this.kiadasi_ev = kiadasi_ev;
+    }
+    
+    @Override
+    public String toString() {
+        return "Konyv{" + "cim=" + cim + ", szerzo1=" + szerzo1 + ", szerzo2=" + szerzo2 + ", kiadasi_ev=" + kiadasi_ev + '}';
+    }
+
     public static void main(String[] args) {
         
     }
--- src/modell/Konyvtar.java
+++ src/modell/Konyvtar.java
@@ -1,5 +1,20 @@
 package modell;
 
+import java.util.ArrayList;
+
+
 public class Konyvtar {
+    private static ArrayList<Konyv> konyvek;
     
+    public static void main(String[] args) {
+        Konyv k1 = new Konyv("Ready Player one", "Ernest Cline", "", 2011);
+        Konyv k2 = new Konyv("Harry Potter és a bölcsek köve", "J. K. Rowling", "", 2022);
+        Konyv k3 = new Konyv("A Piros Alma", "Zöld Alma", "Piros Eper", 2023);
+        
+        konyvek.add(k1);
+        konyvek.add(k2);
+        konyvek.add(k3);
+        
+        System.out.println(konyvek);
+    }
 }
